fe1d8fe0db10563cd145cd5f596ef4434aa2a3bc
luanli27 <luanli27@163.com>
2019-01-03 18:44:36 +0800

    设置图片资源参数,改为sprite

---
+++ Client/Assets/Scripts/Editor/ImgFormatCover.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class ImgFormatCover
+{
+    [MenuItem("Tools/ImgCoverToSprite")]
+    public static void CoverImgToSprite()
+    {
+        Object[] targetObjs = Selection.objects;
+        foreach (var targetObj in targetObjs)
+        {
+            if (targetObj != null && targetObj is Texture)
+            {
+                string path = AssetDatabase.GetAssetPath(targetObj);
+
+                TextureImporter texture = AssetImporter.GetAtPath(path) as TextureImporter;
+                texture.textureType = TextureImporterType.Sprite;
+                texture.spritePixelsPerUnit = 1;
+                texture.filterMode = FilterMode.Trilinear;
+                texture.mipmapEnabled = false;
+                texture.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+                AssetDatabase.ImportAsset(path);
+            }
+        }
+    }
+}
